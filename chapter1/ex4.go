80c24e8fdae9d2a5507a4ba368e4569d579102f1
André Pinheiro <andrepdpinheiro@gmail.com>
2017-03-22 19:36:56 +0000

    Solved exercise 4

---
+++ chapter1/ex4.go
@@ -0,0 +1,26 @@
+package main
+
+func IsPermutationPalindrome(s string) bool {
+	// Allocates and initializes a hash map data structure and returns a pointer to it
+	freqTable := make(map[int]int)
+
+	for i := 'a'; i <= 'z'; i++ {
+		freqTable[int(i)] = 0
+	}
+
+	for _, c := range s {
+		freqTable[int(c)]++
+	}
+
+	foundOdd := false
+	for _, c := range s {
+		if freqTable[int(c)]%2 == 1 {
+			if foundOdd {
+				return false
+			}
+			foundOdd = true
+		}
+	}
+
+	return true
+}
