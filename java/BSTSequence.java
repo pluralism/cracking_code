7e8590c1f7dbf87fa1661f9b1a3586125fbf1f99
André Pinheiro <andrepdpinheiro@gmail.com>
2017-07-22 01:31:12 +0100

    Update BSTSequence.java

--- java/BSTSequence.java
+++ java/BSTSequence.java
@@ -1,36 +1,68 @@
 import java.util.*;
 
-public class BSTSequence {
+class BSTSequence {
     private TreeNode root;
+    private ArrayList<ArrayList<TreeNode>> res = new ArrayList<>();
 
-    public BSTSequence() {
+    BSTSequence() {
         root = new TreeNode(10);
-        root.left = new TreeNode(5);
-        root.right = new TreeNode(15);
-        root.left.right = new TreeNode(7);
-        root.left.left = new TreeNode(3);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        //root.left = new TreeNode(5);
+        //root.right = new TreeNode(15);
+        //root.left.right = new TreeNode(7);
+        //root.left.left = new TreeNode(3);
     }
 
-    public void allPermutations(ArrayList<TreeNode> nodes, int start, int end) {
-        if(start == end) {
-            System.out.println(nodes);
-        } else {
+    private ArrayList<ArrayList<TreeNode>> allPermutations(ArrayList<TreeNode> nodes, int start, int end) {
+        if(start == end)
+            res.add((ArrayList<TreeNode>) nodes.clone());
+        else {
             for(int j = start; j <= end; j++) {
                 Collections.swap(nodes, start, j);
                 allPermutations(nodes, start + 1, end);
                 Collections.swap(nodes, start, j);
             }
         }
+        return res;
     }
 
-    private void buildSequences(HashMap<Integer, ArrayList<TreeNode>> levels) {
-
+    private ArrayList<ArrayList<TreeNode>> mergeSequences(ArrayList<ArrayList<TreeNode>> first, ArrayList<ArrayList<TreeNode>> second) {
+        ArrayList<ArrayList<TreeNode>> results = new ArrayList<>();
+        for(ArrayList<TreeNode> ff : first) {
+            for(ArrayList<TreeNode> ss : second) {
+                ArrayList<TreeNode> tmp = new ArrayList<>();
+                tmp.addAll(ff);
+                tmp.addAll(ss);
+                results.add(tmp);
+            }
+        }
+        return results;
     }
 
-    public void allSequences() {
+    private ArrayList<ArrayList<TreeNode>> buildSequences(HashMap<Integer, ArrayList<TreeNode>> levels) {
+        ArrayList<ArrayList<TreeNode>> results = new ArrayList<>();
+        HashMap<Integer, ArrayList<ArrayList<TreeNode>>> resLevels = new HashMap<>();
+        Set<Integer> keys = levels.keySet();
+
+        for(Integer level : keys) {
+            results = allPermutations(levels.get(level), 0, levels.get(level).size() - 1);
+            resLevels.put(level, (ArrayList<ArrayList<TreeNode>>) results.clone());
+            results.clear();
+        }
+
+        if(resLevels.size() > 1) {
+            results = mergeSequences(resLevels.get(0), resLevels.get(1));
+            for(int i = 2; i < resLevels.size(); i++)
+                results = mergeSequences(results, resLevels.get(i));
+        }
+
+        return results;
+    }
+
+    void allSequences() {
         HashSet<TreeNode> visited = new HashSet<>();
         HashMap<Integer, ArrayList<TreeNode>> levels = new HashMap<>();
-        ArrayList<ArrayList<TreeNode>> nodesLevel = new ArrayList<>();
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
         root.level = 0;
@@ -38,11 +70,10 @@ public class BSTSequence {
 
         while (!q.isEmpty()) {
             TreeNode tmp = q.remove();
-            if(levels.containsKey(tmp.level)) {
+            if(levels.containsKey(tmp.level))
                 levels.get(tmp.level).add(tmp);
-            } else {
+            else
                 levels.put(tmp.level, new ArrayList<>(Arrays.asList(tmp)));
-            }
 
             if(tmp.left != null && !visited.contains(tmp.left)) {
                 q.add(tmp.left);
@@ -55,5 +86,6 @@ public class BSTSequence {
                 visited.add(tmp.right);
             }
         }
+        buildSequences(levels);
     }
 }
